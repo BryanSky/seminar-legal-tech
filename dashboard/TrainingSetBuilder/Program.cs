f0c10a6867abee4b29f380731c075a4dcf97ea92
Benedict Lindner <benedictlindner@outlook.com>
2018-11-25 17:56:20 +0100

    Finished TrainigSetBuilder

--- dashboard/TrainingSetBuilder/Program.cs
+++ dashboard/TrainingSetBuilder/Program.cs
@@ -1,27 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace TrainingSetBuilder {
     class Program {
 
-        private static string file = "";
+        private static string dir = "C:\\Users\\bened\\Desktop\\TrainingFiles";
 
         static void Main(string[] args) {
-            List<string> result = new List<string>();
-            string fullText = ReadFullFile(file);
-            MatchCollection matches = Regex.Matches(fullText, "{\\/rtlch\\/fcs1 \\/af1\\/afs19 \\/ltrch\\/fcs0 \\/fs19\\/lang1024\\/langfe1024\\/highlight([0-9])\\/noproof\\/langnp1024\\/langfenp1024\\/insrsid15405279\\/charrsid([0-9]*) ([A-Z]*[a-z]*)}");
-            foreach (var match in matches) {
-                string rm = match.ToString().Replace("{\\rtlch\\fcs1 \\af1\\afs19 \\ltrch\\fcs0 \\fs19\\lang1024\\langfe1024\\", "");
-                rm = rm.Replace("\\/noproof\\/langnp1024\\/langfenp1024\\/insrsid15405279\\/charrsid", ";");
-                string[] parts = rm.Split(';');
-                string formatNumber = parts[0].Replace("highlight", "");
-                string content = parts[1].Replace("}", "").Substring(8);
-                result.Add(content + "\t" + formatNumber);
+            string[] allFiles = null;
+            if (Directory.Exists(dir)) {
+                allFiles = Directory.GetFiles(dir);
             }
+            if (allFiles != null) {
+                foreach (var file in allFiles) {
+                    List<string> result = new List<string>();
+                    string fullText = ReadFullFile(file);
+                    int index = 0;
+                    while (index > -1) {
+                        index = fullText.IndexOf("highlight7", ++index);
+                        if (index > -1) {
+                            string value = ExtractWord(fullText, index);
+                            result.Add(value + "\tPerson");
+                        }
+                    }
+
+                    index = 0;
+                    while (index > -1) {
+                        index = fullText.IndexOf("highlight3", ++index);
+                        if (index > -1) {
+                            string value = ExtractWord(fullText, index);
+                            result.Add(value + "\tLocation");
+                        }
+                    }
+
+                    index = 0;
+                    while (index > -1) {
+                        index = fullText.IndexOf("highlight4", ++index);
+                        if (index > -1) {
+                            string value = ExtractWord(fullText, index);
+                            result.Add(value + "\tOrganisation");
+                        }
+                    }
+                    //MatchCollection matches = Regex.Matches(fullText, "{\\/rtlch\\/fcs1 \\/af1\\/afs19 \\/ltrch\\/fcs0 \\/fs19\\/lang1024\\/langfe1024\\/highlight7\\/noproof\\/langnp1024\\/langfenp1024\\/insrsid15405279\\/charrsid+([0-9]*) ([A-Z]*[a-z]*)+}");
+                    WriteResultToFile(result, file);
+                }
+            }
+        }
+
+        private static void WriteResultToFile(List<string> result, string s) {
+            string filename = s.Replace(".rtf", ".tsv");
+            System.IO.File.WriteAllLines(filename, result);
+        }
+
+        private static string ExtractWord(string fullText, int index) {
+            int origIndex = index;
+            int startIndex = fullText.IndexOf('{', index-1);
+            int endIndex = -1;
+            endIndex = fullText.IndexOf('}', index-1);
+            while (startIndex > origIndex) {
+                startIndex = fullText.IndexOf('{', index--);
+            }
+            string formatedString = fullText.Substring(startIndex, endIndex - startIndex);
+            startIndex = formatedString.IndexOf("charrsid");
+            //return formatedString.Substring(startIndex + 7, (formatedString.Length - (startIndex + 7)));
+            return Regex.Replace(formatedString.Substring(startIndex + 7, (formatedString.Length - (startIndex + 7))), "d[0-9]*", "");
         }
 
         private static string ReadFullFile(string filename) {
