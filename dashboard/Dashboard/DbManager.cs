775e14f17f18dc86bc035c877e7a62f2ebf7a221
Benedict Lindner <benedictlindner@outlook.com>
2018-11-25 21:57:48 +0100

    Changed dashboards data model

--- dashboard/Dashboard/DbManager.cs
+++ dashboard/Dashboard/DbManager.cs
@@ -9,9 +9,9 @@ using Dashboard.Entities;
 namespace Dashboard {
     public class DbManager {
 
-        protected static string connectionString = "";
+        protected static string ConnectionString = "Server=.\\SQLExpress;Database=legalDB;User Id=myUsername;Password=myPassword;";
 
-        public List<ResultTableRow> GetData(string tagRegex, string valueRegex) {
+        public static List<ResultTableRow> GetData(string tagRegex, string valueRegex) {
             List<ResultTableRow> results = new List<ResultTableRow>();
             string sql = "SELECT nE.Tag nE.[Value], cR.Filename " +
                          "FROM NamedEntity nE " +
@@ -21,7 +21,7 @@ namespace Dashboard {
                          "on cre.CRID = cr.ID " +
                          "WHERE nE.Tag LIKE '" + tagRegex + "' " +
                          "OR nE.[Value] LIKE '" + valueRegex + "'";
-            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
             using (SqlCommand command = new SqlCommand(sql, connection)) {
                 connection.Open();
                 using (SqlDataReader reader = command.ExecuteReader()) {
@@ -37,6 +37,55 @@ namespace Dashboard {
                 }
             }
             return results;
+        }
+
+        public static List<ResultTableRow> GetData(string sql) {
+            return new List<ResultTableRow>();
+        }
+
+        public List<string> LoadPersons() {
+            List<string> persons = new List<string>();
+            string sql = "SELECT DISTINCT [Value] FROM NamedEntity WHERE Tag = 'PERSON'";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection)) {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()){
+                        persons.Add(reader["Value"].ToString());
+                    }
+                }
+            }
+            return persons;
+        }
+
+        public List<string> LoadOrganisations() {
+            List<string> organisations = new List<string>();
+            string sql = "SELECT DISTINCT [Value] FROM NamedEntity WHERE Tag = 'ORGANISATION'";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection)) {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) { 
+                        organisations.Add(reader["Value"].ToString());
+                    }
+                }
+            }
+            return organisations;
+        }
+
+        public List<string> LoadLocations() {
+            List<string> locations = new List<string>();
+            string sql = "SELECT DISTINCT [Value] FROM NamedEntity WHERE Tag = 'LOCATION'";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection)) {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader()) {
+                    while (reader.Read()) {
+                        locations.Add(reader["Value"].ToString());
+                    }
+                }
+            }
+            return locations;
         }
     }
 }
--- dashboard/Dashboard/Models/StartupViewModel.cs
+++ dashboard/Dashboard/Models/StartupViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,16 +9,53 @@ using Dashboard.Entities;
 
 namespace Dashboard.Models
 {
-    public class StartupViewModel
-    {
-        public List<string> klaegerList = new List<string>();
-        public List<string> themeList = new List<string>();
-        public List<string> targetList = new List<string>();
-        public List<string> tagList = new List<string>();
-        public List<ResultTableRow> results = new List<ResultTableRow>();
+    public class StartupViewModel {
+
+        private static string TYPE_PERSON = "Pers";
+        private static string TYPE_ORGANISATION = "Org";
+        private static string TYPE_LOCATION = "Loc";
+
+        public List<string> PersonList = new List<string>();
+        public List<string> OrganisationList = new List<string>();
+        public List<string> LocationList = new List<string>();
+        public List<string> TagList = new List<string>();
+        public List<ResultTableRow> Results = new List<ResultTableRow>();
 
         public void LoadData() {
+            //get values for person, organisation and location
+            //LoadFilteredData();
+        }
 
+        public void LoadFilteredData(string person, string organisation, string location) {
+            if (person == null) {
+                LoadFilteredData(organisation, TYPE_ORGANISATION, location, TYPE_LOCATION);
+            } else if (organisation == null) {
+                LoadFilteredData(person, TYPE_PERSON, location, TYPE_LOCATION);
+            }else if (location == null) {
+                LoadFilteredData(person, TYPE_PERSON, organisation, TYPE_ORGANISATION);
+            }
+            else {
+                string sql = "SELECT * FROM <> WHERE ";
+                Results = DbManager.GetData(sql);
+            }
+        }
+
+        private void LoadFilteredData(string e1, string e1Type, string e2, string e2Type) {
+            if (e1 == null) {
+                LoadFilteredData(e2, e2Type);
+            }else if (e2 == null) {
+                LoadFilteredData(e1, e1Type);
+            }
+            else {
+                string sql = "";
+                Results = DbManager.GetData(sql);
+            }
+        }
+
+        private void LoadFilteredData(string e, string eType) {
+            if (e == null) return;
+            string sql = "";
+            Results = DbManager.GetData(sql);
         }
     }
 }
