6ce7e1662aa76edac686f50886edc316c23a3d79
Benedict Lindner <benedictlindner@outlook.com>
2018-12-09 19:23:16 +0100

    Mocked dashboard

--- dashboard/Dashboard/Models/StartupViewModel.cs
+++ dashboard/Dashboard/Models/StartupViewModel.cs
@@ -21,9 +21,50 @@ namespace Dashboard.Models
         public List<string> TagList = new List<string>();
         public List<ResultTableRow> Results = new List<ResultTableRow>();
 
+        public StartupViewModel() {
+            LoadData();
+        }
+
         public void LoadData() {
+            PersonList = new List<string> {"wählen", "Kläger"};
+            OrganisationList = new List<string> {"wählen", "Finanzamt", "Microsoft"};
+            LocationList = new List<string> {"wählen", "München", "Deutschland", "Österreich"};
             //get values for person, organisation and location
             //LoadFilteredData();
+            ResultTableRow rtRow1 = new ResultTableRow {
+                Klaeger = "Organträger einer GmbH",
+                Target = "Finanzamt",
+                Theme = "Erstattungszinsen für zu Unrecht gezahle Umsatzsteuer"
+            };
+            Results.Add(rtRow1);
+            ResultTableRow rtRow2 = new ResultTableRow
+            {
+                Klaeger = "Kapitalgesellschaft (GmbH)",
+                Target = "Finanzamt",
+                Theme = "Offenbare Unrichtigkeit nach § 129 AO bei fehlender Aufnahme eines Vorbehaltsvermerks"
+            };
+            Results.Add(rtRow2);
+            ResultTableRow rtRow3 = new ResultTableRow
+            {
+                Klaeger = "Klägerin",
+                Target = "Finanzamt",
+                Theme = "Bewertung der Anteile an einer GmbH nach dem Stuttgarter Verfahren"
+            };
+            Results.Add(rtRow3);
+            ResultTableRow rtRow4 = new ResultTableRow
+            {
+                Klaeger = "Eheleute",
+                Target = "Finanzamt",
+                Theme = "Anerkennung von Betriebsausgaben"
+            };
+            Results.Add(rtRow4);
+            ResultTableRow rtRow5 = new ResultTableRow
+            {
+                Klaeger = "Kapitalgesellschaft",
+                Target = "Finanzamt",
+                Theme = "Anforderung an die Kassenbuchführung einer Diskothek"
+            };
+            Results.Add(rtRow5);
         }
 
         public void LoadFilteredData(string person, string organisation, string location) {
